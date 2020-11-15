6400b2758c71b5afc4554fa5e392bb367d4b7417
SMNIK <masood.milestone@studio.unibo.it>
2020-11-15 14:39:14 -0800

    1

--- plot.py
+++ plot.py
@@ -10,10 +10,16 @@ import matplotlib.pyplot as plt
 
 # first = pd.read_excel(<address and name of your file>,<name of the sheet>)
 first = pd.read_excel(r'/Book1.xlsx', 'sheet1')
-first = first.iloc[500:4000]
-plt.plot(first['x'],first['y'])
+
+# x = first['<name of the column>'][<index from>:<to>]
+x = first['x'][500:4000]
+y = first['y'][500:4000]
 
 plt.xlabel('Time (\u03BC s) \n Set of pulses collected at constant d=0.35cm, by varying the sweeping voltage $V_{s}$')
 plt.ylabel('Voltage (v)')
 plt.title('non-fit')
-plt.legend(["14.4v","20.9v","28.1v","36.4v","44.7v"], fontsize=10, loc='upper right')
+plt.legend(["14.4v"], fontsize=10, loc='upper right')
+
+n = len(x)
+mean = sum(x*y)/n
+alpha = sum(y*(x-mean)**2)/n
